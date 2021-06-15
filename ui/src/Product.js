839dede3e32b3ce31899e337f65808629611df49
Mark DeArmond <mark.dearmond@kemper.com>
2021-06-15 11:37:02 -0500

    Moved product into own component

---
+++ ui/src/Product.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from 'react';
+import './Products.css';
+import Card from 'react-bootstrap/Card'
+import Table from 'react-bootstrap/Table'
+import axios from 'axios';
+import Moment from 'react-moment';
+
+function Product(props) {
+
+  const [properties, setProperties] = useState([])
+
+  useEffect(() => {
+    axios.get(`/api/v1/product_properties?search=${props.productId}`)
+    .then(res => {
+      const arr = res.data.map(property => {
+        return {id: property.property.id, name: property.property.name, value: property.value}
+      })
+      setProperties(arr)
+    })
+    .catch(err => console.log(err))
+  }, [])
+
+  return (
+    <Card>
+      <Card.Body>
+        <Card.Title>{props.attributes.name}</Card.Title>
+        <strong>UPC</strong>
+        <p>{props.attributes.upc}</p>
+        <strong>Available On</strong>
+        <p>
+          <Moment format="MM/DD/YYYY">{props.attributes.available_on}</Moment>
+        </p>
+        <Card.Title>Properties</Card.Title>
+        <Table striped bordered>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Value</th>
+              </tr>
+            </thead>
+            <tbody>
+              {properties.map(property => (
+                <tr key={property.id}>
+                  <td>{property.name}</td>
+                  <td>{property.value}</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default Product;
